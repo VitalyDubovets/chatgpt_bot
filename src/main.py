cc487893a5fca5a86f7c1d1ac8f032f194738388
vitaly_dubovets <dubovetsvitaliy@gmail.com>
2023-02-03 23:42:20 +0300

    Fixed a hardcode api key

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ from src.web.handlers.start import send_welcome
 def create_dispatcher() -> Dispatcher:
     configure_logging(log_level=settings.LOG_LEVEL, log_format=settings.LOG_FORMAT)
 
-    openai.api_key = "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"
+    openai.api_key = settings.OPENAI_API_TOKEN
 
     bot = Bot(token=settings.TG_TOKEN)
     # storage = RedisStorage2(host=settings.REDIS_HOST, port=settings.REDIS_PORT, password=settings.REDIS_PASSWORD)
